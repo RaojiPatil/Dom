ce2e5e04d45d208360a76e0a45a72487dd262493
Raoji Patil <raojipatil22@gmail.com>
2021-10-19 21:26:07 +0530

    Import /Export assignment

---
+++ Dom/Import Export/components/navbar.js
@@ -0,0 +1,14 @@
+function navbar() {
+    return ` <div id="navbar">
+    <h3><a href="#">home</a></h3>
+    <div id="options">
+        <h3><a href="jewellery.html">jewellery</a></h3>
+
+        <h3><a href="Electronics.html">Electronics</a></h3>
+        <h3><a href="#">Signin</a></h3>
+        <h3><a href="#">Signup</a></h3>
+    </div>
+</div>`
+}
+
+export default navbar
