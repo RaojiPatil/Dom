91753b51023c3edecd046e5f4c892b42a165d3eb
Raoji Patil <raojipatil22@gmail.com>
2021-11-04 21:16:54 +0530

    change

--- Dom/Import Export/components/navbar.js
+++ Dom/Import Export/components/navbar.js
@@ -1,12 +1,12 @@
 function navbar() {
     return ` 
-    <h3><a href="#">home</a></h3>
+    <h3><a href="">home</a></h3>
     <div id="options">
         <h3><a href="jewellery.html">jewellery</a></h3>
 
         <h3><a href="Electronics.html">Electronics</a></h3>
-        <h3><a href="#">Signin</a></h3>
-        <h3><a href="#">Signup</a></h3>
+        <h3><a href="">Signin</a></h3>
+        <h3><a href="">Signup</a></h3>
     
 </div>`
 }
