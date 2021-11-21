b938dd12235c46d9bd63db84f6ab5c1da982faac
Raoji Patil <raojipatil22@gmail.com>
2021-11-22 00:14:47 +0530

    assignment

--- Dom Unit-3 Async/constructure function/constructure function/task2.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("addtask").addEventListener("click", task);
-
-function task() {
-    var name = document.getElementById("name").value;
-
-    var priority = document.getElementById("priority").value;
-}
